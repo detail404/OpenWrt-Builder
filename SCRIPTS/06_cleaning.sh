735a0bb3f37e7c7e7bc5aa6be53404175a188e2f
KaneGreen <737445366KG@Gmail.com>
2024-01-18 19:32:49 +0800

    update 06_cleaning.sh

--- SCRIPTS/06_cleaning.sh
+++ SCRIPTS/06_cleaning.sh
@@ -1,30 +1,37 @@
 #!/bin/bash
-MYWORKDIR=$(mktemp -d)
-case ${MYOPENWRTTARGET} in
+MYSAVEDIR=$(mktemp -d -p ../)
+if [ -x "$(command -v realpath)" ]; then
+  MYSAVEDIR=$(realpath "$MYSAVEDIR")
+elif [ -x "$(command -v readlink)" ]; then
+  MYSAVEDIR=$(readlink -f "$MYSAVEDIR")
+else
+  MYSAVEDIR=$(pwd)/$MYSAVEDIR
+fi
+case $MYOPENWRTTARGET in
   R2S)
-    mv -f *squashfs* *manifest* ${MYWORKDIR}/
+    mv -f ./*squashfs* ./*manifest* "$MYSAVEDIR"/
     ;;
   x86)
-    mv -f *combined* *manifest* ${MYWORKDIR}/
+    mv -f ./*combined* ./*manifest* "$MYSAVEDIR"/
     ;;
   *)
     echo "Error: Unknown target"
     echo "Please check your environment variable: MYOPENWRTTARGET"
-    echo "Current value is: ${MYOPENWRTTARGET}"
-    rmdir ${MYWORKDIR}
+    echo "Current value is: $MYOPENWRTTARGET"
+    rmdir "$MYSAVEDIR"
     exit 1
     ;;
 esac
 rm -rf ./*
-pushd ${MYWORKDIR} > /dev/null
-  gzip -d *.gz
-  gzip --best --keep *.img
+pushd "$MYSAVEDIR" > /dev/null
+  gzip -d ./*.gz
+  gzip --best --keep ./*.img
   echo && echo
-  sha256sum openwrt* | tee sha256_$(date "+%Y%m%d").hash
-  md5sum    openwrt* | tee    md5_$(date "+%Y%m%d").hash
+  sha256sum openwrt* | tee "sha256_$(date '+%Y%m%d').hash"
+  md5sum    openwrt* | tee    "md5_$(date '+%Y%m%d').hash"
   echo && echo
-  rm -f *.img
+  rm -f ./*.img
 popd > /dev/null
-mv -f ${MYWORKDIR}/* ./
-rmdir ${MYWORKDIR}
+mv -f "$MYSAVEDIR"/* ./
+rmdir "$MYSAVEDIR"
 exit 0
