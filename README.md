4b73fbb37838cd62e52d653dce3c7ffa8082c3b3
Kane Green <737445366KG@Gmail.com>
2025-02-11 11:14:25 +0800

    update openwrt source to 24.10.0

--- README.md
+++ README.md
@@ -1,7 +1,3 @@
-# This is the master branch!
-This repository is going to keep using the term "**master**". It will **never change**.
-I **refuse** to switch to "main".
-
 ## R2S 基于原生 OpenWRT 的固件编译脚本 (AS IS, NO WARRANTY!!!)
 ### 请勿用于商业用途!!!
 **同时也包含了 x86_64 版本**
@@ -26,9 +22,9 @@ https://github.com/KaneGreen/OpenWrt-Builder/actions
 5. R2S 版 sys 灯引导时闪烁，启动后常亮，也是上游的设定，有疑问请联系 OpenWrt 官方社区。
 
 ### 版本信息：
-LUCI版本：OpenWrt-23.05 RC（当日最新）
+LUCI版本：OpenWrt-24.10
 
-其他模块版本：OpenWrt-23.05 RC（当日最新）
+其他模块版本：OpenWrt-24.10
 
 ### 特性及功能：
 1. O2 优化级别。R2S 版核心频率 1.5GHz，SquashFS 格式。x86 版 EXT4 格式，非 UEFI 版本。
--- SCRIPTS/01_get_ready.sh
+++ SCRIPTS/01_get_ready.sh
@@ -1,29 +1,30 @@
 #!/bin/bash
-alias wget="$(which wget) --https-only --retry-connrefused"
 set -e
 set -x
-# get the latest release version of 23.05
-LATESTRELEASE=$(curl -sSf -H 'X-GitHub-Api-Version: 2022-11-28' https://api.github.com/repos/openwrt/openwrt/tags | jq '.[].name' -r | grep -v 'rc' | grep 'v23' | sort -r | head -n 1)
+# get the latest release version of 24.10
+LATESTRELEASE=$(curl -sSf -H 'X-GitHub-Api-Version: 2022-11-28' -H 'Accept: application/vnd.github+json' https://api.github.com/repos/openwrt/openwrt/tags | jq '.[].name' -r | grep -v 'rc' | grep 'v24' | sort -r | head -n 1)
 
 echo "LATESTRELEASE=$LATESTRELEASE" >> ./OPENWRT_GIT_TAG
 
-git clone --single-branch -b 'openwrt-23.05'  --depth 1  https://github.com/openwrt/openwrt.git openwrt_snapshot
+#git clone --single-branch -b 'openwrt-24.10'  --depth 1  https://github.com/openwrt/openwrt.git openwrt_snapshot
 git clone --single-branch -b "$LATESTRELEASE" --depth 1  https://github.com/openwrt/openwrt.git openwrt
 
-rm -rf ./openwrt/package/
-mv -f  ./openwrt_snapshot/package            ./openwrt/package
-mv -f  ./openwrt_snapshot/feeds.conf.default ./openwrt/feeds.conf.default
-rm -rf ./openwrt_snapshot/
-pushd openwrt
-  rm -rf ./package/base-files/ ./package/firmware/ ./package/kernel/ ./package/Makefile
-  git checkout HEAD package/base-files/
-  git checkout HEAD package/firmware/
-  git checkout HEAD package/kernel/
-  git checkout HEAD package/Makefile
-popd
+#rm -rf ./openwrt/package/
+#mv -f  ./openwrt_snapshot/package            ./openwrt/package
+#mv -f  ./openwrt_snapshot/feeds.conf.default ./openwrt/feeds.conf.default
+#rm -rf ./openwrt_snapshot/
+#pushd openwrt
+#  rm -rf ./package/base-files/ ./package/firmware/ ./package/kernel/ ./package/Makefile
+#  git checkout HEAD package/base-files/
+#  git checkout HEAD package/firmware/
+#  git checkout HEAD package/kernel/
+#  git checkout HEAD package/Makefile
+#popd
 
 # 获取额外代码
 git clone -b main          --depth 1 https://github.com/openwrt/openwrt.git            Openwrt_Main/
+sleep 3
+git clone -b master        --depth 1 https://github.com/openwrt/packages.git           Openwrt_PKG_MSTR/
 sleep 3
 git clone -b openwrt-23.05 --depth 1 https://github.com/immortalwrt/immortalwrt.git    Immortalwrt_2305/
 sleep 3
@@ -41,8 +42,6 @@ git clone -b main          --depth 1 https://github.com/Lienol/openwrt-package.g
 sleep 3
 git clone -b master        --depth 1 https://github.com/QiuSimons/OpenWrt-Add.git      OpenWrt-Add/
 sleep 3
-git clone -b master        --depth 1 https://github.com/openwrt/packages.git           Openwrt_PKG_MSTR/
-sleep 3
 git clone -b main          --depth 1 https://github.com/jjm2473/openwrt-third.git      Jjm2473_PACKAGES/
 sleep 3
 git clone -b main          --depth 1 https://github.com/fw876/helloworld.git           SSRP_SRC/
@@ -51,5 +50,4 @@ git clone -b v5            --depth 1 https://github.com/sbwml/openwrt_helloworld
 sleep 3
 git clone -b main          --depth 1 https://github.com/xiaorouji/openwrt-passwall-packages.git Passwall_PKG/
 
-unalias wget
 exit 0
